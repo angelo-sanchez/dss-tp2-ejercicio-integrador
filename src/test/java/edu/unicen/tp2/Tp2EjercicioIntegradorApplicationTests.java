dfdbed12b68ff86d473a1cc1ce42bed9c9b2e3b7
Angelo Sanchez <angelos@unitech.com.ar>
2024-09-19 19:03:16 -0300

    Initial commit

---
+++ src/test/java/edu/unicen/tp2/Tp2EjercicioIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unicen.tp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2EjercicioIntegradorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
