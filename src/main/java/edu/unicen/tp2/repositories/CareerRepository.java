c48536191a7094e39e1f63fa433d71e120dd6a31
jerex3 <jmanuel@sinergiasoluciones.com.ar>
2024-09-24 20:55:40 -0300

    Implemente metodo findCareerWithStudentCount en CareerRepository
    Cambie tipo de retorno en CareerRepository

--- src/main/java/edu/unicen/tp2/repositories/CareerRepository.java
+++ src/main/java/edu/unicen/tp2/repositories/CareerRepository.java
@@ -2,10 +2,11 @@ package edu.unicen.tp2.repositories;
 
 import java.util.List;
 
+import edu.unicen.tp2.dto.CareerInscriptsDTO;
 import edu.unicen.tp2.schema.Career;
 
 public interface CareerRepository {
     public Career findById(long id);
 
-    public List<Object> findCareersWithStudentCount();
+    public List<CareerInscriptsDTO> findCareersWithStudentCount();
 }
--- src/main/java/edu/unicen/tp2/repositories/impl/postgres/CareerRepositoryPostgres.java
+++ src/main/java/edu/unicen/tp2/repositories/impl/postgres/CareerRepositoryPostgres.java
@@ -1,8 +1,12 @@
 package edu.unicen.tp2.repositories.impl.postgres;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import edu.unicen.tp2.dto.CareerInscriptsDTO;
 import edu.unicen.tp2.repositories.CareerRepository;
 import edu.unicen.tp2.schema.Career;
 
@@ -21,9 +25,24 @@ public class CareerRepositoryPostgres implements CareerRepository {
     }
 
     @Override
-    public List<Object> findCareersWithStudentCount() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findCareersWithStudentCount'");
+    public List<CareerInscriptsDTO> findCareersWithStudentCount() {
+        String hql = "SELECT c, COUNT(sc) as count FROM Career c "
+            + "LEFT JOIN StudentCareer sc ON c.id = sc.career.id "
+            + "GROUP BY c.id "
+            + "ORDER BY COUNT(sc) DESC";
+
+        Query<Object[]> query = session.createQuery(hql, Object[].class);
+        List<Object[]> results = query.list();
+
+        List<CareerInscriptsDTO> dtos = new ArrayList<>();
+
+        for (Object[] result : results) {
+            Career career = (Career) result[0];
+            long count = (long) result[1];
+            dtos.add(new CareerInscriptsDTO(career, count));
+        }
+        
+        return dtos;
     }
     
 }
