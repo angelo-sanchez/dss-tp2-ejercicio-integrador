4ad7ed4f0ae74adbc7b19e588e1ce4a60c9f3511
jerex3 <jmanuel@sinergiasoluciones.com.ar>
2024-10-16 18:12:31 -0300

    Corregi tipo del ID de studentCareer

--- src/main/java/edu/unicen/tp2/repositories/StudentCareerRepository.java
+++ src/main/java/edu/unicen/tp2/repositories/StudentCareerRepository.java
@@ -3,7 +3,8 @@ package edu.unicen.tp2.repositories;
 import edu.unicen.tp2.schema.Career;
 import edu.unicen.tp2.schema.Student;
 import edu.unicen.tp2.schema.StudentCareer;
+import edu.unicen.tp2.schema.StudentCareerId;
 
-public interface StudentCareerRepository extends BaseRepository<StudentCareer, Long> {
+public interface StudentCareerRepository extends BaseRepository<StudentCareer, StudentCareerId> {
     StudentCareer save(Student student, Career career);
 }
